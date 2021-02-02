a2bffb7c9995145ef521528e166acfe6cce71a69
Entspannter <lars.masanneck@web.de>
2021-02-02 13:53:12 +0100

    Update addnew.py

--- Web app build v01/Try1 Flask/FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/addnew.py
+++ Web app build v01/Try1 Flask/FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/addnew.py
@@ -386,7 +386,7 @@ def add_pic():
         surgery.pictures = image_string
 
         
-        db.session.update()
+        db.session.commit()
 
         flash("Picture was added!")
         session.pop("curr_id_surg")
